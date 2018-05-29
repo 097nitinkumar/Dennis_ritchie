a61bd20586160e7253bb36fdf41ea77297b82617
Nitin kumar <36824328+097nitinkumar@users.noreply.github.com>
2018-05-29 23:37:41 +0530

    Update 1.7.c

--- 1/1.7.c
+++ 1/1.7.c
@@ -1,21 +1,6 @@
 #include <stdio.h>
 void main()
 {
-	FILE *fp = fopen("test.txt", "r+");
-	int nl=0,bl=0,ts=0;
-	char c;
-	for (;(c = getc(fp))!=EOF;)
-	{
-		switch(c)
-		{
-			case ' ' : 	bl++;
-						break;
-			case '\t' : ts++;
-						break;
-			case '\n' : nl++;
-						break; 
-		}
-	}
-	printf("Blank space:%d Tab space:%d Newline:%d \n",bl,ts,nl);
-
+	char c=EOF;
+	printf("%d %c\n",c,c);
 }
